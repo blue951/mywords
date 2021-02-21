c8e5e3e4a3067af5a41767e0b062638ea4f47d4b
cainiao <san09080aaa@gmail.com>
2021-02-21 15:38:04 +0800

    以前是用函数实现登录功能，改成用类来实现，继承自django自带的view类；

--- MxOnline/apps/users/views.py
+++ MxOnline/apps/users/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
+from django.views.generic.base import View
 
 from .models import UserProfile
 
@@ -29,3 +30,18 @@ def my_login(request):
             return render(request, "login.html", {"msg": "用户名或密码错误"})
     elif request.method == "GET":
         return render(request, "login.html", {})
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "login.html", {})
+
+    def post(self, request):
+        user_name = request.POST.get("username", "")
+        pass_word = request.POST.get("password", "")
+        user = authenticate(username=user_name, password=pass_word)
+        if user is not None:
+            login(request, user)
+            return render(request, "index.html")
+        else:
+            return render(request, "login.html", {"msg": "用户名或密码错误"})
