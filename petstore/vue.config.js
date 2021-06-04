069ddd55c7606aacb357ed77fc2d5cc129f58f65
blue951 <san09090aaa@gmail.com>
2021-06-04 08:59:52 +0800

    add words;

--- petstore/vue.config.js
+++ petstore/vue.config.js
@@ -6,4 +6,19 @@ module.exports={
             errors: false
         }
     },
+    //处理跨域问题
+    dev: {
+        proxyTable: {
+          '/api': {
+            target: 'http://425.0.100.100:8888/',//设置你调用的接口域名和端口号 别忘了加http
+            changeOrigin: true,//如果需要跨域
+            pathRewrite: {
+              '^/api': '/'
+                    //这里理解成用面的地址，
+                    // 后面组件中我们掉接口时直接用api代替 比如我要调
+                    // 用'http://425.0.100.100:8888/user/add'，直接写‘/api/user/add’即可
+            }
+          }
+        }
+    }
 }
---
+++ 练习/js文件内的js引用/a.js
@@ -0,0 +1,2 @@
+console.log("a.js");
+document.write('<script type="text/javascript" src="./b.js" charset="UTF-8"></script>');
